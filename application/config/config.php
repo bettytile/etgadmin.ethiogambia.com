6fc1022549696ec2f2c740a3058c1d86ed90c839
HP PC <betelhemtilahun@gmail.com>
2021-10-14 12:49:50 +0300

    port added

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$root = "http://18.218.91.39/etg_pip/";
+$root = "http://18.218.91.39/etg_pi/";
 // $root .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 $config['base_url'] = $root;
 
